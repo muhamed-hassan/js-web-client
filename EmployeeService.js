db58f881cac7dea23cbee0d7d14759b59c9e5f6f
muhamed-hassan <mohamed.hassan.work@gmail.com>
2023-11-11 07:06:46 +0200

    add CSS

--- EmployeeService.js
+++ EmployeeService.js
@@ -1,25 +1,29 @@
 function findById() {
+    var response;
     try {       
         var requestHeaders = new Map();
         requestHeaders.set("Accept", "application/json");
-        var response = doGet("http://localhost:8080/v2/employees/1", requestHeaders);        
+        response = doGet("http://localhost:8080/v2/employees/1", requestHeaders);        
         var parsedResponse = JSON.parse(response);
         var savedEmployee = new SavedEmployee();
         savedEmployee.id = parsedResponse.id;
         savedEmployee.name = parsedResponse.name;
-        savedEmployee.title = parsedResponse.title;
-        console.log("EmployeeService.findById() response:: " + response);
+        savedEmployee.title = parsedResponse.title;        
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.findById()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.findById()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }
+    return "EmployeeService.findById() response:: " + response;
 }
 
 function findByPage() {
+    var response;
     try {       
         var requestHeaders = new Map();
         requestHeaders.set("Accept", "application/json");
-        var response = doGet("http://localhost:8080/v2/employees?pageNumber=1&pageSize=10", requestHeaders);        
+        response = doGet("http://localhost:8080/v2/employees?pageNumber=1&pageSize=10", requestHeaders);        
         var parsedResponse = JSON.parse(response);
         var collectedElements = new Array();
         for (var cursor = 0; cursor < parsedResponse.length; cursor++) {
@@ -29,11 +33,13 @@ function findByPage() {
             savedEmployee.title = parsedResponse[cursor].title;
             collectedElements.push(savedEmployee);            
         }
-        console.log("EmployeeService.findByPage() response:: " + response);
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.findByPage()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.findByPage()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }
+    return "EmployeeService.findByPage() response:: " + response;
 }
 
 function getWithServerError() {
@@ -42,8 +48,10 @@ function getWithServerError() {
         requestHeaders.set("Accept", "application/json");
         doGet("http://localhost:8080/v2/employees/server_error", requestHeaders);
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.getWithServerError()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.getWithServerError()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }
 }
 
@@ -51,34 +59,39 @@ function getWithServerError() {
 /* ****************************************************************************************************************** */
 
 function save() {
+    var requestPayload;
     try {       
         var requestHeaders = new Map();
         requestHeaders.set("Content-type", "application/json");
         var newEmployee = new NewEmployee();
         newEmployee.name = "sample name";
         newEmployee.title = "sample title";
-        var requestPayload = JSON.stringify(newEmployee);
-        console.log("EmployeeService.save() requestPayload:: " + requestPayload);
+        requestPayload = JSON.stringify(newEmployee);
         doPost("http://localhost:8080/v2/employees", requestHeaders, requestPayload);
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.save()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.save()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }
+    return "EmployeeService.save() requestPayload:: " + requestPayload;
 }
 
 function saveWithViolatingPayloadValidations() {
+    var requestPayload;
     try {       
         var requestHeaders = new Map();
         requestHeaders.set("Content-type", "application/json");
         var newEmployee = new NewEmployee();
         newEmployee.name = "";
         newEmployee.title = "sample title";
-        var requestPayload = JSON.stringify(newEmployee);
-        console.log("EmployeeService.saveWithViolatingPayloadValidations() requestPayload:: " + requestPayload);
+        requestPayload = JSON.stringify(newEmployee);
         doPost("http://localhost:8080/v2/employees", requestHeaders, requestPayload);
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.saveWithViolatingPayloadValidations()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.saveWithViolatingPayloadValidations()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }
 }
 
@@ -92,8 +105,10 @@ function postWithServerError() {
         var requestPayload = JSON.stringify(newEmployee);
         doPost("http://localhost:8080/v2/employees/server_error", requestHeaders, requestPayload);
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.postWithServerError()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.postWithServerError()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }    
 }
 
@@ -103,19 +118,23 @@ function postWithServerError() {
 function deleteById() {
     try {       
         doDelete("http://localhost:8080/v2/employees/51");
-        console.log("Delete is done for employee with ID 51");
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.deleteById()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.deleteById()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }
+    return "Delete is done for employee with ID 51";
 }
 
 function deleteWithServerError() {
     try {       
         doDelete("http://localhost:8080/v2/employees/server_error");
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.deleteWithServerError()");
-        console.error(error + "\n\n");        
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.deleteWithServerError()";
+        console.error(errMsg);
+        console.error(error + "\n\n"); 
+        return errMsg;       
     }
 }
 
@@ -123,19 +142,22 @@ function deleteWithServerError() {
 /* ****************************************************************************************************************** */
 
 function updateById() {
+    var requestPayload;
     try {       
         var requestHeaders = new Map();
         requestHeaders.set("Content-type", "application/json");
         var newEmployee = new NewEmployee();
         newEmployee.name = "sample name";
         newEmployee.title = "sample title";
-        var requestPayload = JSON.stringify(newEmployee);
-        console.log("EmployeeService.updateById() requestPayload:: " + requestPayload);
+        requestPayload = JSON.stringify(newEmployee);
         doPut("http://localhost:8080/v2/employees/91", requestHeaders, requestPayload);
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.updateById()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.updateById()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }
+    return "EmployeeService.updateById() requestPayload:: " + requestPayload;
 }
 
 function updateByIdWithViolatingPayloadValidations() {
@@ -146,11 +168,12 @@ function updateByIdWithViolatingPayloadValidations() {
         newEmployee.name = "";
         newEmployee.title = "sample title";
         var requestPayload = JSON.stringify(newEmployee);
-        console.log("EmployeeService.updateByIdWithViolatingPayloadValidations() requestPayload:: " + requestPayload);
         doPut("http://localhost:8080/v2/employees/91", requestHeaders, requestPayload);
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.updateByIdWithViolatingPayloadValidations()");
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.updateByIdWithViolatingPayloadValidations()";
+        console.error(errMsg);
         console.error(error + "\n\n");        
+        return errMsg;
     }
 }
 
@@ -164,7 +187,9 @@ function putWithServerError() {
         var requestPayload = JSON.stringify(newEmployee);
         doPut("http://localhost:8080/v2/employees/server_error", requestHeaders, requestPayload);
     } catch (error) {
-        console.error("Encountered an error during communicating with the backend. SOURCE::EmployeeService.putWithServerError()");
-        console.error(error + "\n\n");        
+        var errMsg = "Encountered an error during communicating with the backend. SOURCE::EmployeeService.putWithServerError()";
+        console.error(errMsg);
+        console.error(error + "\n\n");  
+        return errMsg;
     }    
 }
--- controller.js
+++ controller.js
@@ -1,33 +1,48 @@
-function launcher() {
-    
+function launcher() {    
+ 
     // GET tests
-    console.log("START of GET tests");
-    findById();
-    findByPage();
-    getWithServerError();
-    console.log("END of GET tests");
-    console.log("#############################################################################");
-
-    // POST tests
-    console.log("START of POST tests");
-    save();
-    saveWithViolatingPayloadValidations();
-    postWithServerError();
-    console.log("END of POST tests");
-    console.log("#############################################################################");
-
+    var collectedOutput = "<div style=\"font-weight: bold;\">START of GET tests</div>";
+    var outputOfFindById = findById();
+    collectedOutput += "<div style=\"margin-left: 20px;\">> " + outputOfFindById + "</div>";
+    var outputOfFindByPage = findByPage();
+    collectedOutput += "<div style=\"margin-left: 20px;\">> " + outputOfFindByPage + "</div>";
+    var outputOfGetWithServerError = getWithServerError();
+    collectedOutput += "<div style=\"color: red; margin-left: 20px;\">> " + outputOfGetWithServerError + "</div>";
+    collectedOutput += "<div style=\"font-weight: bold;\">END of GET tests</div>";
+    collectedOutput += "<div style=\"font-weight: bold;\">#############################################################################</div>";
+    
+    // POST tests    
+    collectedOutput += "<div style=\"font-weight: bold; margin-top: 5px;\">START of POST tests</div>";
+    var outputOfSave = save();
+    collectedOutput += "<div style=\"margin-left: 20px;\">> " + outputOfSave + "</div>";
+    var outputOfSaveWithViolatingPayloadValidations = saveWithViolatingPayloadValidations();
+    collectedOutput += "<div style=\"color: red; margin-left: 20px;\">> " + outputOfSaveWithViolatingPayloadValidations + "</div>";
+    var outputOfPostWithServerError = postWithServerError();
+    collectedOutput += "<div style=\"color: red; margin-left: 20px;\">> " + outputOfPostWithServerError + "</div>";
+    collectedOutput += "<div style=\"font-weight: bold;\">END of POST tests</div>";
+    collectedOutput += "<div style=\"font-weight: bold;\">#############################################################################</div>";
+    
     // DELETE tests
-    console.log("START of DELETE tests");
-    deleteById();
-    deleteWithServerError();
-    console.log("END of DELETE tests");
-    console.log("#############################################################################");
+    collectedOutput += "<div style=\"font-weight: bold; margin-top: 5px;\">START of DELETE tests</div>";
+    var outputOfDeleteById = deleteById();
+    collectedOutput += "<div style=\"margin-left: 20px;\">> " + outputOfDeleteById + "</div>";
+    var outputOfDeleteWithServerError = deleteWithServerError();
+    collectedOutput += "<div style=\"color: red; margin-left: 20px;\">> " + outputOfDeleteWithServerError + "</div>";
+    collectedOutput += "<div style=\"font-weight: bold;\">END of DELETE tests</div>";
+    collectedOutput += "<div style=\"font-weight: bold;\">#############################################################################</div>";
 
     // PUT tests
-    console.log("START of PUT tests");
-    updateById();
-    updateByIdWithViolatingPayloadValidations();
-    putWithServerError();
-    console.log("END of PUT tests");
-    console.log("#############################################################################");
+    collectedOutput += "<div style=\"font-weight: bold; margin-top: 5px;\">START of PUT tests</div>";
+    var outputOfUpdateById = updateById();
+    collectedOutput += "<div style=\"margin-left: 20px;\">> " + outputOfUpdateById + "</div>";
+    var outputOfUpdateByIdWithViolatingPayloadValidations = updateByIdWithViolatingPayloadValidations();
+    collectedOutput += "<div style=\"color: red; margin-left: 20px;\">> " + outputOfUpdateByIdWithViolatingPayloadValidations + "</div>";
+    var outputOfPutWithServerError = putWithServerError();
+    collectedOutput += "<div style=\"color: red; margin-left: 20px;\">> " + outputOfPutWithServerError + "</div>";
+    collectedOutput += "<div style=\"font-weight: bold;\">END of PUT tests</div>";
+    collectedOutput += "<div style=\"font-weight: bold;\">#############################################################################</div>";
+
+    collectedOutput += "<div style=\"font-weight: bold; margin-top: 30px;\">Tests are done successfully 🚀</div>";
+
+    document.getElementById("experimentOutput").innerHTML = collectedOutput;
 }
